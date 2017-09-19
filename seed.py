92efc206f9a69e6cc1a469f1fe87e33994835183
Hackbright Student <no-reply@hackbrightacademy.com>
2017-09-18 17:58:41 -0700

    Finished up to reading data from seed files.

---
+++ seed.py
@@ -0,0 +1,89 @@
+"""Utility file to seed ratings database from MovieLens data in seed_data/"""
+
+from sqlalchemy import func
+from model import User, Rating, Movie
+# from model import Rating
+# from model import Movie
+
+from model import connect_to_db, db
+from server import app
+
+
+def load_users():
+    """Load users from u.user into database."""
+
+    print "Users"
+
+    # Delete all rows in table, so if we need to run this a second time,
+    # we won't be trying to add duplicate users
+    User.query.delete()
+
+    # Read u.user file and insert data
+    for row in open("seed_data/u.user"):
+        row = row.rstrip()
+        user_id, age, gender, occupation, zipcode = row.split("|")
+
+        user = User(user_id=user_id,
+                    age=age,
+                    zipcode=zipcode)
+
+        # We need to add to the session or it won't ever be stored
+        db.session.add(user)
+
+
+    # Once we're done, we should commit our work
+    db.session.commit()
+
+
+def load_movies():
+    """Load movies from u.item into database."""
+    print 'movies'
+    test = db.session
+
+    Movie.query.delete()
+
+    for row in open("seed_data/u.item"):
+        row = row.rstrip()
+        clean = row.split("|")
+        if clean[1] == "unknown":
+            del clean
+        elif clean[1] in test:
+            db.session.delete(movie) # how do we access the session raaawrgh
+        else:
+            movie = Movie(movie_id=clean[0],
+                          title=clean[1][:-7],
+                          released_at=clean[2],
+                          imdb_url=clean[4][:80])
+            #how do we deal with duplicate and unknown entries?
+        db.session.add(movie)
+
+    db.session.commit()
+
+def load_ratings():
+    """Load ratings from u.data into database."""
+
+    pass
+def set_val_user_id():
+    """Set value for the next user_id after seeding database"""
+
+    # Get the Max user_id in the database
+    result = db.session.query(func.max(User.user_id)).one()
+    max_id = int(result[0])
+
+    # Set the value for the next user_id to be max_id + 1
+    query = "SELECT setval('users_user_id_seq', :new_id)"
+    db.session.execute(query, {'new_id': max_id + 1})
+    db.session.commit()
+
+
+if __name__ == "__main__":
+    connect_to_db(app)
+
+    # In case tables haven't been created, create them
+    db.create_all()
+
+    # Import different types of data
+    load_users()
+    load_movies()
+ #   load_ratings()
+    set_val_user_id()
