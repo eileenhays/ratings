a6b4f598d14c13c8ef4d8d18c0b99b6a9b585ef1
Hackbright Student <no-reply@hackbrightacademy.com>
2017-09-19 18:04:13 -0700

    populated db, created templates, and start registration form

--- server.py
+++ server.py
@@ -2,10 +2,11 @@
 
 from jinja2 import StrictUndefined
 
-from flask import Flask, jsonify
+from flask import (Flask, jsonify, render_template, redirect, request,
+                   flash, session)
 from flask_debugtoolbar import DebugToolbarExtension
 
-from model import connect_to_db, db
+from model import connect_to_db, db, User, Rating, Movie
 
 
 app = Flask(__name__)
@@ -22,8 +23,38 @@ app.jinja_env.undefined = StrictUndefined
 @app.route('/')
 def index():
     """Homepage."""
-    a = jsonify([1,3])
-    return a
+
+    return render_template("homepage.html")
+
+
+@app.route("/users")
+def user_list():
+    """Show list of users."""
+
+    users = User.query.all()
+    return render_template("user_list.html", users=users)
+
+
+@app.route("/registration")
+def show_registration():
+    """Shows registration form."""
+
+    return render_template("registration.html")
+
+
+@app.route("/user-data", methods=["POST"])
+def new_user():
+    """Input new user into database."""
+
+    email = request.form.get("email")
+    password = request.form.get("password")
+
+    # if db.session.query(User.email).filter_by(User.email=email) != None:
+    #     return "There was already an account registered by this email."
+
+    user = User(email=email, password=password)
+    db.session.add(user)
+    db.session.commit()
 
 
 if __name__ == "__main__":
@@ -38,5 +69,5 @@ if __name__ == "__main__":
     DebugToolbarExtension(app)
 
 
-    
+
     app.run(port=5000, host='0.0.0.0')
